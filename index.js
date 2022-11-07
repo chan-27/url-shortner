493ac773a750c6c1e431f4a62993cc358c519c48
Sai Chandan Kadarla <chandankadarla2722002@gmail.com>
2022-11-07 13:02:02 +0530

    link to youtube test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http
   .createServer((req, res) => {
-    res.writeHead(301, { Location: "http://www.google.com" });
+    res.writeHead(301, { Location: "http://www.youtube.com" });
     res.end();
   })
   .listen(process.env.PORT || 8000);
